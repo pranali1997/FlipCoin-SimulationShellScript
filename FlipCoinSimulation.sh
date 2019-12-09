37a3c30c92bad693b3f7265dfd32811a4ac95629
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-09 16:24:48 +0530

    added : dictionary and calculated percentage of head and tail of singlet

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,11 +2,26 @@
 
 echo "WELCOME TO FLIP COIN SIMULATION"
 
-randomFlip=$((RANDOM%2))
+read -p "enter the number of times you want to flip the coin" flip
 
-if [ $randomFlip -eq 1 ]
-then
-	echo "Head"
-else
-	echo "tail"
-fi
+head=0
+tail=0
+declare -A combinationDict
+
+for((i=1;i<=flip;i++))
+do
+	randomFlip=$((RANDOM%2))
+	if [ $randomFlip -eq 1 ]
+	then
+		head=$(($head+1))
+		combinationDict[$i]=Head
+	else
+		tail=$(($tail+1))
+		combinationDict[$i]=Tail
+	fi
+done
+total=$(($head+$tail))
+headPer=$((100*$head/$total))
+tailPer=$((100*$tail/$total))
+echo "head percentage :"$headPer
+echo "tail percentage :"$tailPer
