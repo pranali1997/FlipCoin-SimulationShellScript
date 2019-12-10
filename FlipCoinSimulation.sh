00207ce2f5066a00cf47273a20e34a8fd64b65e0
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-10 16:45:32 +0530

    Added : main function, used switch case

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -4,11 +4,14 @@ echo "WELCOME TO FLIP COIN SIMULATION"
 
 read -p "enter the number of times you want to flip the coin" flip
 
+#varibles
 head=0
 tail=0
 counter=0
+
 declare -A combinationDict
-declare -A singletDict
+declare -A resultDict
+
 function getPercentage()
 {
 	for((j=0; j<$flip; j++))
@@ -30,10 +33,7 @@ function getPercentage()
 		combinationDict["$j"]=$coinValue
 	done
 	total=$flip
-	headPer=$((100*$head/$total))
-	tailPer=$((100*$tail/$total))
-	echo ${combinationDict[@]}
-
+	echo ${combinationDict[@]} | sort -n
 }
 
 function singlet()
@@ -47,14 +47,15 @@ function singlet()
 	do
 		if [ $i == "H" ]
 		then
-			H=$(($head+1))
+			H=$(($H+1))
 		else
-			T=$(($tail+1))
+			T=$(($T+1))
 		fi
 	done
 
-	echo H=$((100*$head/$flip))
-	echo T=$((100*$tail/$flip))
+	resultDict["H"]=$((100*$H/$flip))
+	resultDict["T"]=$((100*$T/$flip))
+	echo ${resultDict[@]} | sort -n
 }
 
 
@@ -81,10 +82,70 @@ function doublet()
 			TH=$(($HT+1))
 		fi
 	done
-	echo HH=$((100*HH/$flip))
-	echo TT=$((100*TT/$flip))
-	echo HT=$((100*HT/$flip))
-	echo TH=$((100*TH/$flip))
+	resultDict["HH"]=$((100*HH/$flip))
+	resultDict["TT"]=$((100*TT/$flip))
+	resultDict["HT"]=$((100*HT/$flip))
+	resultDict["TH"]=$((100*TH/$flip))
+	echo ${resultDict[@]} | sort -n
 }
-doublet
 
+
+function triplet()
+{
+	HHH=0 THH=0 HTH=0 HHT=0 TTH=0 THT=0 HTT=0 TTT=0
+	triple=""
+	coin=3
+	tripleValue=$(getPercentage $coin)
+
+	for i in $tripleValue
+	do
+		if [ $i == "HHH" ]
+		then
+			HHH=$(($HHH+1))
+		elif [ $i == "THH" ]
+		then
+			THH=$(($THH+1))
+		elif [ $i == "HTH" ]
+		then
+			HTH=$(($HTH+1))
+		elif [ $i == "HHT" ]
+		then
+			HHT=$(($HTT+1))
+		elif [ $i == "TTH" ]
+		then
+			TTH=$(($TTH+1))
+		elif [ $i = "THT" ]
+		then
+			THT=$(($THT+1))
+		elif [ $i == "HTT" ]
+		then
+			HTT=$(($HTT+1))
+		elif [ $i == "TTT" ]
+		then
+			TTT=$(($TTT+1))
+		fi
+	done
+	resultDict["HHH"]=$((100*$HHH/$flip))
+	resultDict["THH"]=$((100*$THH/$flip))
+	resultDict["HTH"]=$((100*$HTH/$flip))
+	resultDict["HHT"]=$((100*$HHT/$flip))
+	resultDict["TTH"]=$((100*$TTH/$flip))
+	resultDict["THT"]=$((100*$THT/$flip))
+	resultDict["HTT"]=$((100*$HTT/$flip))
+	resultDict["TTT"]=$((100*$TTT/$flip))
+	echo ${resultDict[@]} | sort -n
+}
+
+function main()
+{
+	echo "1.singlet"
+	echo "2.doublet"
+	echo "3.triplet"
+	read -p "Enter your choice " choice
+	case $choice in
+		1)echo result=$(singlet);;
+		2)echo result=$(doublet);;
+		3)echo result=$(triplet);;
+	esac
+}
+main
